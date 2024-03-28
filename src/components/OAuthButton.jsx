7e12451a9d016ceecd4b15d2fb9224759a969f07
rajput999 <rajput9997647419@gmail.com>
2024-03-28 15:59:42 +0530

    oath integeration

--- src/components/OAuthButton.jsx
+++ src/components/OAuthButton.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import GoogleLogo from '../assets/Images/google.svg';
+import Axios from 'axios'
 
 const GoogleOAuthButton = () => {
+  const handleclick = async ()=>{
+    window.open("http://localhost:8080/auth/google",'_self');
+    const response = await Axios.get("http://localhost:8080/auth/google");
+  }
   return (
     <div className="flex items-center justify-center">
       <button
         className="flex bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring focus:ring-red-400"
         onClick={() => {
-          // Handle Google sign-in
+          handleclick();
         }}
       >
         <img className="w-6 h-6 mr-2 fill-current" src={GoogleLogo}/>
