4941df2eb3aa87e2f9ccb1afbb8500b12d69f5fc
shaunak-09 <shaunakghosh09@kgpian.iitkgp.ac.in>
2024-03-30 01:28:47 +0530

    admin and sidebar setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './media.css'
+import './App.css'
+// import './media.css'
 import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from 'react-router-dom'
 import HeroSection from './components/herosection.jsx'
 import Plans from './components/AddToFavourite.jsx/plans.jsx'
@@ -11,14 +12,15 @@ import LoginPage from './components/LoginPage.jsx'
 import SignupPage from './components/SignupPage.jsx'
 import MySpace from './components/AddToFavourite.jsx/mySpace.jsx'
 import SearchPage from "./components/searchPage.jsx"
-// import MoviePlayback from './MoviePlayback.jsx/MoviePlayback.jsx'
 import Success from './components/sucess.jsx'
-import Failure from './components/failure.jsx'
+// import MoviePlayback from './MoviePlayback.jsx/MoviePlayback.jsx'
+
+
 
 
 // Admin Imports 
 // import Home from "./admin/Home.jsx";
-// import Banners from './components/admin_dash.jsx'
+import Banners from './components/admin_dash.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -31,9 +33,9 @@ const router = createBrowserRouter(
       <Route path='/profile' element={<MySpace />}/>
       <Route path="searchPage"  element={<SearchPage/>}/>
       {/* <Route path="/moviePlayback"  element={<MoviePlayback/>}/> */}
-      {/* <Route path="/admin"  element={<Banners/>}/> */}
+      <Route path="/admin"  element={<Banners/>}/>
       <Route path="/success/:amt"  element={<Success/>}/>
-      <Route path="/failure"  element={<Failure/>}/>
+      {/* <Route path="/failure"  element={<Failure/>}/> */}
 
       {/* Admin Routes */}
 
